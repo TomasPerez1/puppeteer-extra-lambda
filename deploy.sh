7ab3c5a22f3aa698dd70731ca93c6dc4978eee88
tomasPerez <tomasperez.henry@gmail.com>
2024-10-27 13:05:56 -0300

    add property

--- deploy.sh
+++ deploy.sh
@@ -6,11 +6,11 @@ npx tsc
 echo "command - aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 156041417839.dkr.ecr.us-east-1.amazonaws.com"
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 156041417839.dkr.ecr.us-east-1.amazonaws.com
 
-echo "command - docker build --platform linux/amd64 --progress=plain -t scraper ."
-docker build --platform linux/amd64 --progress=plain -t scraper .
+echo "command - docker build --platform linux/amd64 --progress=plain -t property-scraper ."
+docker build --platform linux/amd64 --progress=plain -t property-scraper .
 
-echo "command - docker tag scraper:latest 156041417839.dkr.ecr.us-east-1.amazonaws.com/scraper:latest"
-docker tag scraper:latest 156041417839.dkr.ecr.us-east-1.amazonaws.com/scraper:latest
+echo "command - docker tag property-scraper:latest 156041417839.dkr.ecr.us-east-1.amazonaws.com/property-scraper:latest"
+docker tag property-scraper:latest 156041417839.dkr.ecr.us-east-1.amazonaws.com/property-scraper:latest
 
-echo "command - docker push 156041417839.dkr.ecr.us-east-1.amazonaws.com/scraper:latest"
-docker push 156041417839.dkr.ecr.us-east-1.amazonaws.com/scraper:latest
+echo "command - docker push 156041417839.dkr.ecr.us-east-1.amazonaws.com/property-scraper:latest"
+docker push 156041417839.dkr.ecr.us-east-1.amazonaws.com/property-scraper:latest
